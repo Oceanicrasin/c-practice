ec0e8c2f5632d8b9366969b1d208c69f13c33709
Oceanicrasin <107171751+Oceanicrasin@users.noreply.github.com>
2022-09-20 08:15:30 +0100

    Create scanf.c

---
+++ scanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char character;
+    char string[500];
+    scanf("%c", &character);
+    scanf("%s", string);
+    printf("Your character: %c\n", character);
+    printf("Your string: %s\n", string);
+    return 0;
+}
